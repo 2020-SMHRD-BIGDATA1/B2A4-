65bc1495cc7472b4f26fedf97c3f29ce58b0a7d7
SMHRD <SMHRD@DESKTOP-E3M0DBA>
2020-08-14 09:22:26 +0900

    0814 9:22, 멤버 테이블 생성

---
+++ B2A4TEAM/Table.sql
@@ -0,0 +1,13 @@
+drop table member
+
+create table gae_member(
+mem_name varchar2(100) not null,
+mem_mail varchar2(100),
+mem_pw varchar2(100) not null,
+mem_nick varchar2(100),
+mem_gender varchar2(50) not null,
+mem_birth varchar2(100) not null,
+mem_tel varchar2(200) not null,
+mem_addr varchar2(200) not null,
+constraint name_nick_pk primary key(mem_name, mem_nick)
+)
